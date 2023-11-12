a9aa7895391f855afa82b26e0d326741322d15b9
mpuig15 <puig.ruiz.miquel@gmail.com>
2023-11-12 22:54:25 +0100

    Update database.js

--- database.js
+++ database.js
@@ -32,7 +32,7 @@ export async function createTask(taskEx, creatorEx) {
     console.log(`La task es ${taskEx}`);
     console.log(`El creator es ${creatorEx}`);
     await pool.query(`
-    INSERT INTO tasks (task, creator, created) values ($1, $2, 2023-10-10`, taskEx, creatorEx)
+    INSERT INTO tasks (task, creator, created) values ($1, $2, '2023-10-10');`, taskEx, creatorEx)
     
 }
 
